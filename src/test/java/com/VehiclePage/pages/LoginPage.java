0503373270575c36e0f7dbec1f1824909d4c1524
okk33sch <korayozbex@gmail.com>
2022-04-02 15:25:23 -0400

    LoginPage.java added

---
+++ src/test/java/com/VehiclePage/pages/LoginPage.java
@@ -0,0 +1,35 @@
+package com.VehiclePage.pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import com.VehiclePage.utilities.Driver;
+
+public class LoginPage {
+
+    public LoginPage(){
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
+
+
+    @FindBy(id = "prependedInput")
+    public WebElement userInput;
+
+    @FindBy(id = "prependedInput2")
+    public WebElement passwordInput;
+
+    @FindBy(id="_submit")
+    public WebElement loginButton;
+
+
+    public void login(String username, String password) {
+        this.userInput.sendKeys(username);
+        this.passwordInput.sendKeys(password);
+        this.loginButton.click();
+    }
+
+
+
+
+
+}
